f982494c38cee583c796c8fd8996a41735677158
Josh Castro <castrojoshua805@gmail.com>
2020-10-20 17:41:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,13 +200,5 @@ def player_stats(player_name)
 end 
 
 def big_shoe_rebounds 
-  max_player = nil
-  game_hash.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
-      max_player = player_hash if player_hash[:shoe_size] > max_player[:shoe_size]
-    end
-  end
-  max_player[:stats][:rebounds]
-end
-
+  
+end 
